50e14b12257b4e330f8cd6921d8b9922383660c2
Nelio Alves <nelio.iftm@gmail.com>
2016-12-17 22:24:30 -0200

    Externalised webmaster email and login details

--- src/main/java/com/devopsbuddy/config/ApplicationConfig.java
+++ src/main/java/com/devopsbuddy/config/ApplicationConfig.java
@@ -1,11 +1,13 @@
 package com.devopsbuddy.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.devopsbuddy.backend.persistence.repositories")
 @EnableTransactionManagement
+@PropertySource("file:///${user.home}/.devopsbuddy/application-common.properties")
 public class ApplicationConfig {
 }
